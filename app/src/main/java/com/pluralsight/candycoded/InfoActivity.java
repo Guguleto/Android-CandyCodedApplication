743fc2194179cc250e6f6fba0e412cc600040ade
Guguleto <64301404+Guguleto@users.noreply.github.com>
2020-09-27 00:03:28 +0200

    createMapIntent method

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -23,12 +23,14 @@ public class InfoActivity extends AppCompatActivity {
                 into(candyStoreImageView);
 
 
+
+
     }
 
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
-    private void createMapIntent(View view){
+    public void createMapIntent(View view){
         Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
         mapIntent.setPackage("com.google.android.apps.maps");
